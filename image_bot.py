2fed1b8254e4ee33ccddf4b82d92f17015296e1f
balu83vs <balu83vs@gmail.com>
2023-12-11 10:49:10 +0300

    image_bot_aiogram

--- image_bot.py
+++ image_bot.py
@@ -1,6 +1,12 @@
 import os
+import asyncio
+import glob
+import random
 import time
-import telebot
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 
 # Укажите токен вашего бота
 TOKEN = 'YOUR_BOT_TOKEN'
@@ -11,42 +17,43 @@ IMAGES_DIRECTORY = 'PATH_TO_IMAGES_DIRECTORY'
 # Укажите период (в секундах) между отправкой картинок
 PERIOD = 60  # Например, раз в минуту
 
-# Создаем экземпляр бота
-bot = telebot.TeleBot(TOKEN)
+# Создаем экземпляры бота и диспетчера
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
 
 # Функция для отправки картинок в канал
-def send_images_to_channel():
+async def send_images_to_channel():
     # Получаем список файлов в директории с картинками
-    images = [image for image in os.listdir(IMAGES_DIRECTORY) if os.path.isfile(os.path.join(IMAGES_DIRECTORY, image))]
+    images = glob.glob(os.path.join(IMAGES_DIRECTORY, '*.jpg')) + glob.glob(os.path.join(IMAGES_DIRECTORY, '*.png'))
     
     # Отправляем каждую картинку в канал
     for image in images:
-        # Полный путь к файлу
-        image_path = os.path.join(IMAGES_DIRECTORY, image)
-        
-        # Отправляем картинку в канал
-        with open(image_path, 'rb') as f:
-            bot.send_photo(CHANNEL_ID, f)
-        
-        # Задержка между отправкой картинок
-        time.sleep(PERIOD)
+        with open(image, 'rb') as photo:
+            await bot.send_photo(CHANNEL_ID, photo)
+            await asyncio.sleep(PERIOD)
+
 
 # Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.reply_to(message, 'Привет! Я бот, который будет отправлять картинки в канал.')
+@dp.message_handler(commands=['start'])
+async def handle_start(message: types.Message):
+    await message.reply('Привет! Я бот, который будет отправлять картинки в канал.')
 
-# Главная функция
-def main():
+
+# Функция, которая будет отправлять картинки в канал с указанным периодом
+async def scheduled(wait_for):
     while True:
-        try:
-            send_images_to_channel()
-        except Exception as e:
-            print(f'Произошла ошибка: {e}')
-        
-        # Задержка перед повторной отправкой картинок
-        time.sleep(PERIOD)
+        await send_images_to_channel()
+        await asyncio.sleep(wait_for)
+
+
+# Функция для запуска бота
+def run_bot():
+    # Запуск диспетчера с заданным интервалом отправки картинок
+    dp.loop.create_task(scheduled(PERIOD))
+    executor.start_polling(dp, skip_updates=True)
+
 
 # Запускаем бота
 if __name__ == '__main__':
-    main()
+    run_bot()
